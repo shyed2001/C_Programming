a2f379ec96a59aaed3fe9d6f8869d00ee4d320ff
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-05-29 02:13:30 +0600

    ok while loop in c
    ok while loop in c

--- C_LanguageFundamentals.c
+++ C_LanguageFundamentals.c
@@ -1677,7 +1677,124 @@ since you are told that there are no more than 50 boxcars.   \n\  " );
 
 
 
+    puts ("  While-loops are perfect for when you want to execute an undetermined number of iterations. ");
 
+    puts ("  Activity: compute budget using a while loop (External resource) ");
+
+    puts ("  Much of the work of a university administration (in addition to managing \n \
+           teachers, researchers, students, courses, etc.) is to ensure the proper \n \
+          functioning of the university and in particular that the accounting job is well done. \n \
+           Once a year, an annual report of expenditures must be made. \n \
+           All expenses for the year have been recorded and classified in a multitude of \n \
+           files and the sum of all these expenses must now be calculated. \n \
+           But no one knows exactly how many different expenses have been made in the past year! \n \
+           Your program will have to read a sequence of positive integers and display their sum. \n \
+            We do not know how many integers there will be, but the sequence always ends with the value -1 \n \
+            (which is not an expense, just an end marker). Example 1 Input 1000 2000 500 -1 Output 3500 \n \
+           Example 2 Input -1  Output 0 Example 3 Input 150 250 350 4500 240 120 -1 Output 5610");
+
+
+
+    double ExpenValue = 0 ;
+
+    double ExpenSum = 0 ;
+
+    puts("Please enter values of expenses and end with -1 as escape character");
+
+    while ( ExpenValue != -1)
+    {
+
+        scanf("%lf", &ExpenValue );
+
+        ExpenSum = ExpenSum + ExpenValue;
+
+    }
+
+    printf("%.0lf \n", ExpenSum+1);
+
+
+    puts( " Write a program to determine - How many days are needed to get Required number of (from user input) signatures. \n \
+         Initially there are n signatures (from user input), Each day every one with a new signature is required to get two other \n \
+         signatures in the next day " );
+
+
+    int signaturesNeeded = 0;
+
+    int day = 0;
+
+    int newSignatures = 0;
+
+    int totalSignatures = 0;
+
+    puts( " Please enter the number of Required signatures " );
+
+    scanf(" %d", &signaturesNeeded  );
+
+    puts( " Please enter the number of Initial signatures.  " );
+
+    scanf(" %d", &newSignatures  );
+
+    totalSignatures =  newSignatures ;
+
+    while (totalSignatures < signaturesNeeded)
+    {
+        day++;
+        newSignatures = 2*newSignatures;
+        printf("Day %d: %d new signatures! ", day, newSignatures);
+        totalSignatures = totalSignatures + newSignatures;
+        printf("Total: %d\n", totalSignatures);
+    }
+
+
+
+    puts( " Activity: controlling an epidemic " );
+
+    puts( " Activity: controlling an epidemic (External resource) " );
+
+
+
+    puts(" In order to be able to better fight various epidemics in a region, the department of medicine of a major university \n \
+          has launched a large study. Researchers are interested in how fast an epidemic spreads, and therefore the speed at which  \n \
+           health measures must be put in place. Your program should first read an integer representing the total population of the area. \n \
+            Knowing that a person was infected on day 1 and that each infected person contaminates two new people every day, \n \
+            you must calculate the day at which the entire population of the area will be infected.    \n \
+            Examples : For a total population of 3 inhabitants, on day 1 a single person is infected. The next day, \n \
+            that person contaminates 2 new people so there are 3 infected people in total. This is the entire population, \n \
+            so it takes 2 days to contaminate the entire population. \n \
+            Input: 3 Output: 2 \n \
+            For a total population of 10 inhabitants, on day 1 a single person is infected. \n \
+             This is followed by 2 new people on the second day for a total of 3 infected people. \n \
+             On the third day,  6 new people are infected for a total of 9 infected people. \n \
+             On the fourth day the last of the 10 people is infected (though the epedemic had the potential to \n \
+            infect 18 people on the fourth day) so your program should output '4'.   Input: 10 Output: 4   \n \  " );
+
+   int CityPopulation = 0;
+
+    int Day = 1;  /// initial contamination.
+
+    int newInfections = 1;
+
+    int totalInfections = 1;
+
+    scanf(" %d", &CityPopulation  );
+
+    // totalInfections =  newInfections ;
+
+    while (totalInfections < CityPopulation )
+
+    {
+
+        newInfections = 2*totalInfections ;
+        /// Be careful that each day, all infected people contaminate two more people, not only the newly infected ones.
+
+        totalInfections = totalInfections + newInfections;
+    /// Each day, all infected people contaminate two other ones, not only the newly infected ones.
+        Day++;
+
+    }
+
+
+        printf("%d", Day);
 
 
 
--- test.c
+++ test.c
@@ -3,7 +3,8 @@
 #include <unistd.h>
 #include <math.h>
 #include <time.h>
-
+#include <time.h>
+#include <stdlib.h>
 #include <stdbool.h>
 int main(void)
 {
@@ -11,201 +12,270 @@ int main(void)
     clock_t tStart = clock();
 
 
-    puts ("  While-loops are perfect for when you want to execute an undetermined number of iterations. ");
 
-    puts ("  Activity: compute budget using a while loop (External resource) ");
+    puts( " Activity: guess my number " );
 
-    puts ("  Much of the work of a university administration (in addition to managing \n \
-           teachers, researchers, students, courses, etc.) is to ensure the proper \n \
-          functioning of the university and in particular that the accounting job is well done. \n \
-           Once a year, an annual report of expenditures must be made. \n \
-           All expenses for the year have been recorded and classified in a multitude of \n \
-           files and the sum of all these expenses must now be calculated. \n \
-           But no one knows exactly how many different expenses have been made in the past year! \n \
-           Your program will have to read a sequence of positive integers and display their sum. \n \
-            We do not know how many integers there will be, but the sequence always ends with the value -1 \n \
-            (which is not an expense, just an end marker). Example 1 Input 1000 2000 500 -1 Output 3500 \n \
-           Example 2 Input -1  Output 0 Example 3 Input 150 250 350 4500 240 120 -1 Output 5610");
+    puts( " Activity: guess my number (External resource) " );
 
 
 
-    double ExpenValue = 0 ;
+    puts( " We would like you to develop a program capable of making a child play automatically the game of \n \
+            \"more or less\" -- the child must try to guess a secret number. The program should respond to each guess with \n \
+            \"it is more\ or \"it is less\" until the child finds the right number. \n \
+           Your program must first read an integer indicating the number that the child will have to find during the game.  \n \
+          Next the program should repeatedly read the player's guesses and display the text \"it is more\" if the child has submitted a smaller  \n \
+           number or \"it is less\" if they have submitted a larger number. Once the correct number is reached, the program should print \n \
+           \"Number of tries needed:\" followed by a new line and the integer number of tries that it took the guesser. \n \
+           Example 1 Input 5  \n \
+           1 2 3 4 5 \n \
+          it is more it is more it is more it is more Number of tries needed: 5 \n \
+          Example 2 Input 10 \n \
+          5 15 8 12 11 10 \n \
+          it is more it is less it is more it is less it is less Number of tries needed: 6 \n \ " );
 
-    double ExpenSum = 0 ;
 
-    puts("Please enter values of expenses and end with -1 as escape character");
 
-    while ( ExpenValue != -1)
-    {
+          int rRandom = 0 ;
+          puts( " please enter a secret number between 1 to 99  to find via guess " );
+          scanf("%d", &rRandom);
 
-        scanf("%lf", &ExpenValue );
+          int UserGuess = 0 ;
 
-        ExpenSum = ExpenSum + ExpenValue;
+          int NumTriesGuess = 1;
 
-    }
+          puts( " please enter a secret number between 1 to 99 as a guess " );
+          scanf("%d", &UserGuess);
+          while ( UserGuess != rRandom )
+          {
 
-    printf("%.0lf \n", ExpenSum+1);
+            NumTriesGuess++;
+            if( UserGuess < rRandom )
+            {
+                puts( "it is more" );
+            }
 
+            else
+            {
+                 puts( "it is less" );
+            }
 
-    puts( " Write a program to determine - How many days are needed to get Required number of (from user input) signatures. \n \
-         Initially there are n signatures (from user input), Each day every one with a new signature is required to get two other \n \
-         signatures in the next day " );
+            puts( " please enter a secret number between 1 to 99 as a guess " );
+            scanf("%d", &UserGuess);
+          }
 
+          printf(" Number of tries needed:\n %d", NumTriesGuess);
 
-    int signaturesNeeded = 0;
 
-    int day = 0;
+          puts( " " );
 
-    int newSignatures = 0;
+          puts( " " );
 
-    int totalSignatures = 0;
+          puts( " " );
 
-    puts( " Please enter the number of Required signatures " );
+          puts( " " );
 
-    scanf(" %d", &signaturesNeeded  );
+          /* random int between 0 and 19
+          int r = rand() % 20;
 
-    puts( " Please enter the number of Initial signatures.  " );
+          */
 
-    scanf(" %d", &newSignatures  );
+          srand(time(NULL));   // Initialization, should only be called once.
+          /// int r = rand();      // Returns a pseudo-random integer between 0 and RAND_MAX.
+          /* random int between 0 and 99 */
 
-    totalSignatures =  newSignatures ;
+          rRandom = rand() % 99;
 
-    while (totalSignatures < signaturesNeeded)
-    {
-        day++;
-        newSignatures = 2*newSignatures;
-        printf("Day %d: %d new signatures! ", day, newSignatures);
-        totalSignatures = totalSignatures + newSignatures;
-        printf("Total: %d\n", totalSignatures);
-    }
+          // int rRandom = 9 ;
 
+          UserGuess = 0 ;
 
 
-    puts( " Activity: controlling an epidemic " );
+          NumTriesGuess = 1;
 
-    puts( " Activity: controlling an epidemic (External resource) " );
+          /// printf(" The secret Number was: %d", rRandom);
 
+          puts( " please enter a number between 0 to 99 as a guess " );
+          scanf("%d", &UserGuess);
+          while ( UserGuess != rRandom )
+          {
 
+            NumTriesGuess++;
+            if( UserGuess < rRandom )
+            {
+                puts( "it is more" );
+            }
 
-    puts(" In order to be able to better fight various epidemics in a region, the department of medicine of a major university \n \
-          has launched a large study. Researchers are interested in how fast an epidemic spreads, and therefore the speed at which  \n \
-           health measures must be put in place. Your program should first read an integer representing the total population of the area. \n \
-            Knowing that a person was infected on day 1 and that each infected person contaminates two new people every day, \n \
-            you must calculate the day at which the entire population of the area will be infected.    \n \
-            Examples : For a total population of 3 inhabitants, on day 1 a single person is infected. The next day, \n \
-            that person contaminates 2 new people so there are 3 infected people in total. This is the entire population, \n \
-            so it takes 2 days to contaminate the entire population. \n \
-            Input: 3 Output: 2 \n \
-            For a total population of 10 inhabitants, on day 1 a single person is infected. \n \
-             This is followed by 2 new people on the second day for a total of 3 infected people. \n \
-             On the third day,  6 new people are infected for a total of 9 infected people. \n \
-             On the fourth day the last of the 10 people is infected (though the epedemic had the potential to \n \
-            infect 18 people on the fourth day) so your program should output '4'.   Input: 10 Output: 4   \n \  " );
+            else
+            {
+                 puts( "it is less" );
+            }
 
-   int CityPopulation = 0;
+            scanf("%d", &UserGuess);
+          }
 
-    int Day = 1;  /// initial contamination.
+          printf(" Number of tries needed: %d", NumTriesGuess);
+                    puts( " " );
 
-    int newInfections = 1;
 
-    int totalInfections = 1;
+          printf(" The secret Number was: %d", rRandom);
 
-    scanf(" %d", &CityPopulation  );
 
-    // totalInfections =  newInfections ;
 
-    while (totalInfections < CityPopulation )
+          puts( "Activity: monitoring a chemical experiment " );
 
-    {
+          puts( "Activity: monitoring a chemical experiment (External resource) " );
 
-        newInfections = 2*totalInfections ;
-        /// Be careful that each day, all infected people contaminate two more people, not only the newly infected ones.
+          puts( "   \n \
+               University chemists have developed a new process for the manufacturing of a drug that heals wounds extremely quickly. \n \
+               The manufacturing process is very lengthy and requires monitoring the chemicals at all times, sometimes for hours! \n \
+               Entrusting this task to a student is not possible; students tend to fall asleep or not pay close attention after a while. \n \
+               Therefore you need to program an automatic device to monitor the manufacturing of the drug. \n \
+               The device measures the temperature every 15 seconds and provides these measurement to your program. \n \
+               Your program should first read two integers representing the minimum and maximum safe temperatures. \n \
+               Next, your program should continuously read temperatures (integers) that are being provided by the device. \n \
+               Once the chemical reaction is complete the device will send a value of -999, \n \
+               indicating to you that temperature readings are done. For each recorded temperature that is in the correct range \n \
+               (it could also be equal to the min or max values), your program should display the text \"Nothing to report\".  \n \
+               But as soon as a temperature reaches an unsafe level your program must display the text \"Alert!\" \n \
+                and stop reading temperatures (although the device may continue sending temperature values). \n \
+              Examples Input: 10 20 \n \
+              15 10 20 0 15 -999 \n \
+                Output: Nothing to report \n \  Nothing to report \n \ Nothing to report \n \   Alert! \n \
+                Input: 0 100 \n \
+                15 50 75 -999 \n \
+                Output: \n \  Nothing to report \n \  Nothing to report \n \   Nothing to report \n \   " );
 
-        totalInfections = totalInfections + newInfections;
-    /// Each day, all infected people contaminate two other ones, not only the newly infected ones.
-        Day++;
+                puts( "This task offers 1 hint : Hint 1 : Use a while loop to continue reading temperatures until \n \
+                     the program reads -999 or an unsafe temperature. " );
 
-    }
 
 
-        printf("%d", Day);
+          int minimumSafeTemperatures = 0;
 
+          int maximumSafeTemperatures = 0;
 
+          int ReadTemp = 0 ;
 
+         puts( " Your should first input two integers representing the minimum and maximum safe temperatures. " );
 
+            scanf("%d", &minimumSafeTemperatures );
 
+            scanf("%d", &maximumSafeTemperatures );
 
+          while ( ReadTemp != -999)
+          {
+                 puts( " Your should integers representing the temperatures, enter -999 to stop. " );
 
+               scanf("%d", &ReadTemp );
 
+            if( ReadTemp >= minimumSafeTemperatures && ReadTemp <= maximumSafeTemperatures )
+            {
+                puts( "Nothing to report" );
+            }
 
+            else if ( ReadTemp == -999)
+            {
+                /// puts( "End of Input" );
 
+                 break;
+            }
 
+            else
+            {
+                 puts( "Alert!" );
 
-         /*
+                 break;
+            }
 
-         The above code can also be executed by the following code
-         #include <stdio.h>
 
-         int main()
-         {
-         int expense;
-         scanf("%d",&expense);
-         int sumOfExpenses = 0;
-         while (expense != -1)
-         {
-            sumOfExpenses = sumOfExpenses + expense;
-            scanf("%d",&expense);
-         }
-         printf("%d\n",sumOfExpenses);
 
-         return 0;
-         }
+          }
 
 
-         */
 
+          }
 
 
+          puts( " " );
 
+          puts( " " );
 
-         /*
+          puts( " " );
 
-         IF-ELSE statement shortcut in C
 
-         C has the following syntax for a shorthand IF-ELSE statement
+          puts( " " );
 
-         (integer == 5) ? (TRUE) : (FALSE);
-         I often find myself requiring only one portion (TRUE or FALSE) of the statement and use this
+          puts( " " );
 
-         (integer == 5) ? (TRUE) : (0);
-         I was just wondering if there was a way to not include the ELSE portion of the statement using this shorthand notation?
+          puts( " " );
 
-         The operator ?: must return a value. If you didn't have the "else" part, what would it return when the boolean
-         expression is false? A sensible default in some other languages may be null, but probably not for C. If you just
-         need to do the "if" and you don't need it to return a value, then typing if is a lot easier.
-         Question is whether we can somehow write the following expression without both then and else parts
 
-         (integer == 5) ? (THENEXPR) : (ELSEEXPR);
-         If you only need the then part you can use &&:
 
-         (integer == 5) && (THENEXPR)
-         If you only need the else part use ||:
+          /*
 
-         (integer == 5) || (ELSEEXPR)
+          The above code can also be executed by the following code
+          #include <stdio.h>
 
-            */
+          int main()
+          {
+          int expense;
+          scanf("%d",&expense);
+          int sumOfExpenses = 0;
+          while (expense != -1)
+          {
+             sumOfExpenses = sumOfExpenses + expense;
+             scanf("%d",&expense);
+          }
+          printf("%d\n",sumOfExpenses);
 
+          return 0;
+          }
 
-         printf(" \n Time taken: %.5f s\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
+          */
 
 
 
-         return 0;
-         system("pause>null");
 
-         /// system() is a library function that makes calls to OS commands/programs. system("pause") calls the "pause"
-         /// command in DOS/Windows. Run the pause command in a command shell to see what it does.
-         /// will cause the black dos window (where you see your output) to be paused untill you press any key.
-         printf("Time taken: %.5fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+          /*
+
+          IF-ELSE statement shortcut in C
+
+          C has the following syntax for a shorthand IF-ELSE statement
+
+          (integer == 5) ? (TRUE) : (FALSE);
+          I often find myself requiring only one portion (TRUE or FALSE) of the statement and use this
+
+          (integer == 5) ? (TRUE) : (0);
+          I was just wondering if there was a way to not include the ELSE portion of the statement using this shorthand notation?
+
+          The operator ?: must return a value. If you didn't have the "else" part, what would it return when the boolean
+          expression is false? A sensible default in some other languages may be null, but probably not for C. If you just
+          need to do the "if" and you don't need it to return a value, then typing if is a lot easier.
+          Question is whether we can somehow write the following expression without both then and else parts
+
+          (integer == 5) ? (THENEXPR) : (ELSEEXPR);
+          If you only need the then part you can use &&:
+
+          (integer == 5) && (THENEXPR)
+          If you only need the else part use ||:
+
+          (integer == 5) || (ELSEEXPR)
+
+             */
+
+
+          printf(" \n Time taken: %.5f s\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+
+
+
+          return 0;
+          system("pause>null");
+
+          /// system() is a library function that makes calls to OS commands/programs. system("pause") calls the "pause"
+          /// command in DOS/Windows. Run the pause command in a command shell to see what it does.
+          /// will cause the black dos window (where you see your output) to be paused untill you press any key.
+          printf("Time taken: %.5fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 }
