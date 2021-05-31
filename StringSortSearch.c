643493e0fa52fef4df79bc22c3233b4d8e2e51fa
shyed2001 <shyed2001@yahoo.com>
2021-05-31 18:35:08 +0600

    ok
    ok

--- StringSortSearch.c
+++ StringSortSearch.c
@@ -1,4 +1,3 @@
-//#include <cs50.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <math.h>
@@ -6,115 +5,168 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+#define maxCount 500
+
 int main(void)
 {
 
     clock_t tStart = clock();
 
 
+    puts( " " );
 
-    puts( " Activity: guess my number " );
+    puts( " " );
 
-    puts( " Activity: guess my number (External resource) " );
-
-
-
-            puts( " please enter a secret number between 1 to 99 as a guess " );
-            scanf("%d", &UserGuess);
+    puts( " " );
 
 
-          printf(" Number of tries needed:\n %d", NumTriesGuess);
+    //! showArray(word, cursors[i])
 
+    char word[51]; //arrray of characters (string)
 
-          puts( " " );
+    printf("Please enter a word within 50 letters/characters Maximum : ");
 
-          puts( " " );
+    scanf("%s", word);
 
-          puts( " " );
+    printf("The word read is: %s.\n", word);
 
-          puts( " " );
+    printf("The characters are: <%c> <%c> <%c> <%c>\n", word[0], word[1], word[2], word[3]);
 
-          puts( " " );
+    word[1] = 'a';
 
-          puts( " " );
-
-          puts( " " );
-
-
-          puts( " " );
-
-          puts( " " );
-
-          puts( " " );
-
-
-
-          /*
-
-          The above code can also be executed by the following code
-          #include <stdio.h>
-
-          int main()
-          {
-          int expense;
-          scanf("%d",&expense);
-          int sumOfExpenses = 0;
-          while (expense != -1)
-          {
-             sumOfExpenses = sumOfExpenses + expense;
-             scanf("%d",&expense);
-          }
-          printf("%d\n",sumOfExpenses);
-
-          return 0;
-          }
-
-
-          */
+    printf("The new word is %s\n", word);
 
 
 
 
+    puts( " Write a C-program that prints out a word as many times as specified. \n \
+               The number of repetitions and the word should be given as input to the program. \n \
+               You may assume that the word has no more than 100 characters (be sure to also reserve space for the null terminator, \0, though!). " );
 
-          /*
+    puts( " Examples Input: \n \
+                    2 Hello \n \
+                    Output: \n \
+                    Hello \n \
+                    Hello \n \ " );
 
-          IF-ELSE statement shortcut in C
+    puts( " Input: 4 thing \n \
+                    Output: \n \
+                    thing \n \
+                    thing \n \
+                    thing \n \
+                    thing  \n \ " );
 
-          C has the following syntax for a shorthand IF-ELSE statement
-
-          (integer == 5) ? (TRUE) : (FALSE);
-          I often find myself requiring only one portion (TRUE or FALSE) of the statement and use this
-
-          (integer == 5) ? (TRUE) : (0);
-          I was just wondering if there was a way to not include the ELSE portion of the statement using this shorthand notation?
-
-          The operator ?: must return a value. If you didn't have the "else" part, what would it return when the boolean
-          expression is false? A sensible default in some other languages may be null, but probably not for C. If you just
-          need to do the "if" and you don't need it to return a value, then typing if is a lot easier.
-          Question is whether we can somehow write the following expression without both then and else parts
-
-          (integer == 5) ? (THENEXPR) : (ELSEEXPR);
-          If you only need the then part you can use &&:
-
-          (integer == 5) && (THENEXPR)
-          If you only need the else part use ||:
-
-          (integer == 5) || (ELSEEXPR)
-
-             */
-
-
-          printf(" \n Time taken: %.5f s\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    puts( " This task offers 1 hint : Hint 1 : Use a for loop to repeat printing the scanned word as many times as necessary. " );
 
 
 
+        int numberRepeat = 0 ;
 
-          return 0;
-          system("pause>null");
+        char TheWord[101];
 
-          /// system() is a library function that makes calls to OS commands/programs. system("pause") calls the "pause"
-          /// command in DOS/Windows. Run the pause command in a command shell to see what it does.
-          /// will cause the black dos window (where you see your output) to be paused untill you press any key.
-          printf("Time taken: %.5fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+         puts("Please enter a word within 100 letters/characters Maximum : ");
+
+         scanf("%d", &numberRepeat);
+
+         scanf("%s", TheWord);
+
+         printf("The word read is: %s.\n", TheWord);
+
+         for (int i = 0 ; i < numberRepeat ; i++ )
+         {
+            printf("%s\n", TheWord);
+         }
+
+
+
+
+    puts( " can we store a sentence??? " );
+    puts( " can we store a sentence??? " );
+
+    puts( " With %s format specifier, scanf will stop at first blank characters, so you can't read and store a sentence (with spaces) with it. \n \
+     There are several ways to do it though, but they are not introduced in this course. \n \
+     One solution is to use fgets function instead of scanf. " );
+
+    puts( " Please enter a sentence with within 500 letters/characters Maximum : " );
+
+
+
+char sen[maxCount];
+
+fgets(sen, maxCount, stdin);
+
+scanf("%[^\n]%*c", sen);
+
+printf("%s",sen);
+
+
+    printf(" \n Time taken: %.5f s\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+
+
+
+    return 0;
+    system("pause>null");
+
+    /// system() is a library function that makes calls to OS commands/programs. system("pause") calls the "pause"
+    /// command in DOS/Windows. Run the pause command in a command shell to see what it does.
+    /// will cause the black dos window (where you see your output) to be paused untill you press any key.
+    printf("Time taken: %.5fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 }
 
+
+/*
+
+The above code can also be executed by the following code
+#include <stdio.h>
+
+int main()
+{
+int expense;
+scanf("%d",&expense);
+int sumOfExpenses = 0;
+while (expense != -1)
+{
+   sumOfExpenses = sumOfExpenses + expense;
+   scanf("%d",&expense);
+}
+printf("%d\n",sumOfExpenses);
+
+return 0;
+}
+
+
+*/
+
+
+
+
+
+/*
+
+IF-ELSE statement shortcut in C
+
+C has the following syntax for a shorthand IF-ELSE statement
+
+(integer == 5) ? (TRUE) : (FALSE);
+I often find myself requiring only one portion (TRUE or FALSE) of the statement and use this
+
+(integer == 5) ? (TRUE) : (0);
+I was just wondering if there was a way to not include the ELSE portion of the statement using this shorthand notation?
+
+The operator ?: must return a value. If you didn't have the "else" part, what would it return when the boolean
+expression is false? A sensible default in some other languages may be null, but probably not for C. If you just
+need to do the "if" and you don't need it to return a value, then typing if is a lot easier.
+Question is whether we can somehow write the following expression without both then and else parts
+
+(integer == 5) ? (THENEXPR) : (ELSEEXPR);
+If you only need the then part you can use &&:
+
+(integer == 5) && (THENEXPR)
+If you only need the else part use ||:
+
+(integer == 5) || (ELSEEXPR)
+
+   */
