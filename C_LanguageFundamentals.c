8629e25a98b29f8a695748cf06c2571e6bb6207c
shyed2001 <shyed2001@yahoo.com>
2021-06-01 19:10:42 +0600

    ok
    ok

--- C_LanguageFundamentals.c
+++ C_LanguageFundamentals.c
@@ -1798,6 +1798,190 @@ since you are told that there are no more than 50 boxcars.   \n\  " );
 
 
 
+            //! showArray(word, cursors[i])
+
+    char word[51]; //arrray of characters (string)
+
+    printf("Please enter a word within 50 letters/characters Maximum : ");
+
+    scanf("%s", word);
+
+    printf("The word read is: %s.\n", word);
+
+    printf("The characters are: <%c> <%c> <%c> <%c>\n", word[0], word[1], word[2], word[3]);
+
+    word[1] = 'a';
+
+    printf("The new word is %s\n", word);
+
+
+
+
+    puts( " Write a C-program that prints out a word as many times as specified. \n \
+               The number of repetitions and the word should be given as input to the program. \n \
+               You may assume that the word has no more than 100 characters (be sure to also reserve space for the null terminator, \0, though!). " );
+
+    puts( " Examples Input: \n \
+                    2 Hello \n \
+                    Output: \n \
+                    Hello \n \
+                    Hello \n \ " );
+
+    puts( " Input: 4 thing \n \
+                    Output: \n \
+                    thing \n \
+                    thing \n \
+                    thing \n \
+                    thing  \n \ " );
+
+    puts( " This task offers 1 hint : Hint 1 : Use a for loop to repeat printing the scanned word as many times as necessary. " );
+
+
+
+    int numberRepeat = 0 ;
+
+    char TheWord[101];
+
+    puts("Please first enter an integer as repetition number and enter a word within 100 letters/characters Maximum : ");
+
+    scanf("%d", &numberRepeat);
+
+    scanf("%s", TheWord);
+
+    printf("The word read is: %s.\n", TheWord);
+
+    for (int i = 0 ; i < numberRepeat ; i++ )
+    {
+        printf("%s\n", TheWord);
+    }
+
+
+
+
+    puts( " can we store a sentence??? " );
+    puts( " can we store a sentence??? " );
+
+    puts( " With %s format specifier, scanf will stop at first blank characters, so you can't read and store a sentence (with spaces) with it. \n \
+     There are several ways to do it though, but they are not introduced in this course. \n \
+     One solution is to use fgets function instead of scanf. " );
+
+    puts( " Please enter a sentence with within 500 letters/characters Maximum : " );
+
+
+
+    char sen[maxCount];
+
+    fgets(sen, maxCount, stdin);
+
+    scanf("%[^\n]%*c", sen);
+
+    printf("%s",sen);
+
+
+
+
+
+    puts( " Working with more than one words " );
+
+    puts( " " );
+
+    puts( " Please input two word of less than 6 letters/characters" );
+
+    char word1[7];
+    char word2[8];
+    scanf("%s%s", word1, word2);
+
+    printf(" You entered %s %s\n", word1, word2);
+
+
+
+
+    puts( " " );
+
+    puts( " " );
+
+    puts( " Replacing word1[3] = '\\0';  word2[2] = '\\0';" );
+
+
+    puts( " Please input two word of less than 6 letters/characters" );
+
+    word1[7];
+    word2[7];
+    scanf("%s%s", word1, word2);
+
+    printf(" You entered %s %s\n", word1, word2);
+
+    puts( " Replacing word1[3] = '\0';  word2[2] = '\0';" );
+    word1[3] = '\0';
+    word2[2] = '\0';
+    printf(" You entered %s %s\n", word1, word2);
+
+
+
+
+
+
+
+    puts( "  Your local public library keeps a record of all of its patrons, \n \
+                 consisting of index cards that hold a person\'s last name followed by their first name \n \
+                  (so that the cards can easily be sorted alphabetically by last name). \n \
+                  Unfortunately a computer error led to incorrectly printed forms last month,\n \
+                  resulting in a number of cards that list the patron's first name followed by their \n \
+                  last name rather than the other way around. Your job is to read these pairs of first and \n \
+                  last names and display them in the correct order (last name followed by first name).  \n \
+                 You may assume that each first and last name has at most 100 characters and does not contain any spaces. \n \
+                 Your program should first read the total number of names (an integer) in order to know how many index cards need to be processed altogether. \n \
+                 Next,  for each index card, your program should read a patron\'s first name and last name and then display these names correctly, \n \
+                 that is on one line, the last name followed by one space, followed by the first name.  \n \
+                  Your program should print the reversed name immediately after reading the patron\'s names \n \
+                  (ie, it should not wait until it has read all of the index cards to begin printing). \n \
+                 Note that, for ease of viewing, the example below shows all of the inputs in one block and all \n \
+                 of the outputs in another block, despite the fact that programmatically these will be interspersed. \n \
+                  \n \
+                 Example ,  Input: 4 Alan Turing  Ada Lovelace  Donald Knuth  Claude Shannon \n \
+                 Output: Turing Alan Lovelace Ada Knuth Donald Shannon Claude    \n \ " );
+
+
+
+    int NumOfContributorNames = 0;
+
+    char FirstContributorName[101];
+    char LastContributorName[101];
+
+    puts( " PLease enter an integer , for number of names to be entererd " );
+
+    scannf ( "%d", &NumOfContributorNames);
+
+
+    for ( i = 0; i < NumOfContributorNames; i++ )
+
+    {
+        puts( " Please enter a name " );
+
+        scannf ( "%s %s", FirstContributorName, LastContributorName );
+
+        printf("%s %s",LastContributorName, FirstContributorName);
+
+    }
+
+
+
+
+    puts( " Find the length of a string " );
+
+
+    puts( " Find the length of a string" );
+
+    char word[30];
+    int i = 0;
+    printf("Please enter a word: ");
+    scanf("%s", word);
+    while (word[i]!='\0')
+        i++;
+    printf("%s has word length %d.\n", word, i);
+
+
+
 
     printf("Time taken: %.5fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
     return 0;
--- StringSortSearch.c
+++ StringSortSearch.c
@@ -22,171 +22,43 @@ int main(void)
     puts( " " );
 
 
-    //! showArray(word, cursors[i])
+    puts( " Activity: even or odd number of letters in a word?" );
 
-    char word[51]; //arrray of characters (string)
+    puts( " Activity: odd even length of string (External resource) " );
 
-    printf("Please enter a word within 50 letters/characters Maximum : ");
 
-    scanf("%s", word);
 
-    printf("The word read is: %s.\n", word);
+    puts( " At the annual meeting of MOOC fans, participants register on the first day of the event in order to receive their name tags, \n \
+         brochures and banquet vouchers. Unfortunately this often results in long lines. In an attempt to speed things up, \n \
+          there are now two people working the registration desk: one person who has the registration materials \n \
+          for those fans whose names contain an odd number of letters, the other for those whose names have an even number of letters. \n \
+            Your job is to write a C-program that will tell a fan which line to stand in. \n \
+            To simplify the program, you may assume that student names are less than 50 characters long and contain no spaces. \n \
+         Your program should output an integer value (1 or 2) depending on whether the fan should join \n \
+         line 1 (name has even number of letters) or line 2 (name has odd number of letters). " );
 
-    printf("The characters are: <%c> <%c> <%c> <%c>\n", word[0], word[1], word[2], word[3]);
+    puts( " Examples Input: Sharrock , Output: 1. Input: Bonfert, Output: 2 " );
 
-    word[1] = 'a';
+    puts( " Remember that strings are null terminated, so you can count characters until you reach the \0 character to find the string's length. " );
 
-    printf("The new word is %s\n", word);
+    puts( " Activity: even or odd number of letters in a word?" );
 
+    puts( " Activity: odd even length of string (External resource) " );
 
 
 
-    puts( " Write a C-program that prints out a word as many times as specified. \n \
-               The number of repetitions and the word should be given as input to the program. \n \
-               You may assume that the word has no more than 100 characters (be sure to also reserve space for the null terminator, \0, though!). " );
+    char wordName[50];
+    int iName = 0;
+    printf("Please enter a Name: ");
+    scanf("%s", wordName);
+    while (wordName[iName]!='\0')
+        iName++;
+    printf("%s has word length %d.\n", wordName,iName);
 
-    puts( " Examples Input: \n \
-                    2 Hello \n \
-                    Output: \n \
-                    Hello \n \
-                    Hello \n \ " );
-
-    puts( " Input: 4 thing \n \
-                    Output: \n \
-                    thing \n \
-                    thing \n \
-                    thing \n \
-                    thing  \n \ " );
-
-    puts( " This task offers 1 hint : Hint 1 : Use a for loop to repeat printing the scanned word as many times as necessary. " );
-
-
-
-        int numberRepeat = 0 ;
-
-        char TheWord[101];
-
-         puts("Please first enter an integer as repetition number and enter a word within 100 letters/characters Maximum : ");
-
-         scanf("%d", &numberRepeat);
-
-         scanf("%s", TheWord);
-
-         printf("The word read is: %s.\n", TheWord);
-
-         for (int i = 0 ; i < numberRepeat ; i++ )
-         {
-            printf("%s\n", TheWord);
-         }
-
-
-
-
-    puts( " can we store a sentence??? " );
-    puts( " can we store a sentence??? " );
-
-    puts( " With %s format specifier, scanf will stop at first blank characters, so you can't read and store a sentence (with spaces) with it. \n \
-     There are several ways to do it though, but they are not introduced in this course. \n \
-     One solution is to use fgets function instead of scanf. " );
-
-    puts( " Please enter a sentence with within 500 letters/characters Maximum : " );
-
-
-
-char sen[maxCount];
-
-fgets(sen, maxCount, stdin);
-
-scanf("%[^\n]%*c", sen);
-
-printf("%s",sen);
-
-
-
-
-
-        puts( " " );
-
-    puts( " " );
-
-    puts( " Please input two word of less than 6 letters/characters" );
-
-    char word1[7];
-    char word2[8];
-    scanf("%s%s", word1, word2);
-
-    printf(" You entered %s %s\n", word1, word2);
-
-
-
-
-        puts( " " );
-
-    puts( " " );
-
-    puts( " Replacing word1[3] = '\\0';  word2[2] = '\\0';" );
-
-
-    puts( " Please input two word of less than 6 letters/characters" );
-
-    word1[7];
-    word2[7];
-    scanf("%s%s", word1, word2);
-
-    printf(" You entered %s %s\n", word1, word2);
-
-    puts( " Replacing word1[3] = '\0';  word2[2] = '\0';" );
-    word1[3] = '\0';
-    word2[2] = '\0';
-    printf(" You entered %s %s\n", word1, word2);
-
-
-
-
-
-
-
-            puts( "  Your local public library keeps a record of all of its patrons, \n \
-                 consisting of index cards that hold a person\'s last name followed by their first name \n \
-                  (so that the cards can easily be sorted alphabetically by last name). \n \
-                  Unfortunately a computer error led to incorrectly printed forms last month,\n \
-                  resulting in a number of cards that list the patron's first name followed by their \n \
-                  last name rather than the other way around. Your job is to read these pairs of first and \n \
-                  last names and display them in the correct order (last name followed by first name).  \n \
-                 You may assume that each first and last name has at most 100 characters and does not contain any spaces. \n \
-                 Your program should first read the total number of names (an integer) in order to know how many index cards need to be processed altogether. \n \
-                 Next,  for each index card, your program should read a patron\'s first name and last name and then display these names correctly, \n \
-                 that is on one line, the last name followed by one space, followed by the first name.  \n \
-                  Your program should print the reversed name immediately after reading the patron\'s names \n \
-                  (ie, it should not wait until it has read all of the index cards to begin printing). \n \
-                 Note that, for ease of viewing, the example below shows all of the inputs in one block and all \n \
-                 of the outputs in another block, despite the fact that programmatically these will be interspersed. \n \
-                  \n \
-                 Example ,  Input: 4 Alan Turing  Ada Lovelace  Donald Knuth  Claude Shannon \n \
-                 Output: Turing Alan Lovelace Ada Knuth Donald Shannon Claude    \n \ " );
-
-
-
-                 int NumOfContributorNames = 0;
-
-                 char FirstContributorName[101];
-                 char LastContributorName[101];
-
-     puts( " PLease enter an integer , for number of names to be entererd " );
-
-     scannf ( "%d", &NumOfContributorNames);
-
-
-     for ( i = 0; i < NumOfContributorNames; i++ )
-
-     {
-         puts( " Please enter a name " );
-
-         scannf ( "%s %s", FirstContributorName, LastContributorName );
-
-         printf("%s %s",LastContributorName, FirstContributorName);
-
-     }
+         if ( iName % 2 == 0)
+            puts( "1" );
+         else
+             puts( "2" );
 
 
 
@@ -194,13 +66,21 @@ printf("%s",sen);
 
     puts( " " );
 
-            puts( " " );
+    puts( " " );
 
     puts( " " );
 
+    puts( " " );
 
+    puts( " " );
 
+    puts( " " );
 
+    puts( " " );
+
+    puts( " " );
+
+    puts( " " );
 
 
 
