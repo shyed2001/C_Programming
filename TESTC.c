863a6ffafc4ba7572c1ea0e0a4b76d0258f63fde
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-06-16 23:16:42 +0600

    ok
    ok

---
+++ TESTC.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main (void)
+{
+  char z;
+  puts("hello world");
+  puts("Use gcc -save-temps testC.c -o testC.exe for getting all the preprossed and object and assembly source files saved with chosen name");
+ puts("press any key to exit");
+  scanf("%c",z);
+  
+  return 0;
+  
+}
