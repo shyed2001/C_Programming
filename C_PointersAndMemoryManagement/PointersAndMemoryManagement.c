2e7ff70dca150cf5cc95d0272d1fa5cf08f982d0
shyed2001 <shyed2001@yahoo.com>
2021-08-16 17:00:45 +0600

    Update PointersAndMemoryManagement.c
    ok

--- C_PointersAndMemoryManagement/PointersAndMemoryManagement.c
+++ C_PointersAndMemoryManagement/PointersAndMemoryManagement.c
@@ -251,6 +251,11 @@ int main(int argc, char* argv[])
 /// Activity: passing array to function to find score (External resource)
 
 
+
+
+
+
+
 /*
 
 You are participating in a game in which players collect points for various solved puzzles.
@@ -339,12 +344,110 @@ Output
     short * t[2] = {a,b};
     setToZero(t);
 
+/* Pointer arithmetics and pointer to pointer  References
+void setToZero(short ** t){
+    **t = *(*t) = 0; //t[0][0]  OR  *(t[0] + 0)
+    *((*t) + 1) = 0;//t[0][1]  OR *(t[0] + 1)
+    *((*t) + 2) = 0;//t[0][2]  OR *(t[0] + 2)
+    *(*(t+1)) = 0;//t[1][0]  OR *(t[1] + 0)
+    *(*(t+1)+1) = 0;//t[1][1]  OR *(t[1] + 1)
+}
+
+*/
+///Unit 4.2: Multidimensional arrays / Working with arrays and pointers
+
+/// Activity: arrays and memory
+
+
+/// Activity: arrays and memory (External resource)
 
 
 
+/*
+You are designing a new cookie recipe and are experimenting with different
+amounts of wet (water, oil) and dry (flour, sugar, cocoa powder) ingredients
+ in order to get the proportions just right. All of these amounts are
+  initially read from the user input, and the code to do so, along with all
+   variable declarations, had already been completed. You are interested in
+    the total amount of wet and dry ingredients used in the recipe as well
+     as the ratio of these two quantities.
+
+     Example
+Input
+10.5 20.2
+30.3 40.4 50.5
+Output
+Total amount of wet ingredients: 30.70 grams.
+Total amount of dry ingredients: 121.20 grams.
+Ratio of wet to dry ingredients: 3.95.
+New water amount: 15.35 grams, new oil amount: 15.35 grams.
+*/
 
 
 
+    double totalWet, totalDry, ratio;
+
+    double wet[2];
+    double dry[3];
+    double * cookie[2] = {wet,dry};
+
+    scanf("%lf%lf", &wet[0], &wet[1]);
+    scanf("%lf%lf%lf", &dry[0], &dry[1], &dry[2]);
+
+    // Add one line here!
+    /* The line you add should use the array cookie (and not the array wet) to
+       find the sum of the wet ingredients of the cookie recipe and store that sum
+       in the variable totalWet. Use only indexed notation to address the cookie
+       array (that is, you need to use two pairs of brackets [..]).
+    */
+
+    totalWet = cookie[0][0] + cookie[0][1] ;
+
+    printf("Total amount of wet ingredients: %.2lf grams.\n", totalWet);
+
+    // Add one line here!
+    /* The line you add should use the array cookie (and not the array dry) to
+       find the sum of the dry ingredients of the cookie recipe and store that sum
+       in the variable totalDry. This time, use only one pair of brackets [..] each
+       time you address the cookie array.
+    */
+
+    totalDry = cookie [1] [0] + cookie [1] [1] + cookie [1] [2];
+
+    printf("Total amount of dry ingredients: %.2lf grams.\n", totalDry);
+    ratio = totalDry/totalWet;
+    printf("Ratio of wet to dry ingredients: %.2lf.\n", ratio);
+
+    // Add two lines here.
+    /* The lines you add should use the array cookie (and not the array wet) to
+       update the amounts of water and oil in your recipe.
+       You believe that any cookie recipe should use equal amounts of water and oil.
+       Without changing the total amount of wet ingredients, update the values for
+       water and oil, using only the array cookie (and not the array wet) so that
+       these amounts will be equal. The easiest way to do so is to assign the value
+       totalWet/2 to both the water and the oil entry. When addressing the array cookie,
+       do not use any brackets at all this time.
+    */
+
+    /// wet[0] = *(*cookie) = **cookie =cookie[0][0]
+
+    *(*cookie) = totalWet/2;
+
+   // wet[1] = cookie[0][1] = *(*(cookie) + 1)
+
+   *(*cookie + 1) = totalWet/2;
+
+    printf("New water amount: %.2lf grams, new oil amount: %.2lf grams.\n", wet[0], wet[1]);
+
+
+/* Pointer arithmetics and pointer to pointer  References
+void setToZero(short ** t){
+    **t = *(*t) = 0; //t[0][0]  OR  *(t[0] + 0)
+    *((*t) + 1) = 0;//t[0][1]  OR *(t[0] + 1)
+    *((*t) + 2) = 0;//t[0][2]  OR *(t[0] + 2)
+    *(*(t+1)) = 0;//t[1][0]  OR *(t[1] + 0)
+    *(*(t+1)+1) = 0;//t[1][1]  OR *(t[1] + 1)
+}
 
 
 
