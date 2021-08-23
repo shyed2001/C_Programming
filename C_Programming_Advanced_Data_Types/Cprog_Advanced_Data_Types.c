98d5083e91ba8392b8051ec12921533e330e570f
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-08-24 02:09:49 +0600

    Update Cprog_Advanced_Data_Types.c
    ok

--- C_Programming_Advanced_Data_Types/Cprog_Advanced_Data_Types.c
+++ C_Programming_Advanced_Data_Types/Cprog_Advanced_Data_Types.c
@@ -81,6 +81,15 @@ struct digit {
 };
 struct digit * createDigit(int dig);
 
+struct student {
+      char name[50];
+      int age;
+      struct student *next;
+};
+
+struct student *createStudent(char studentName[], int studentAge);
+
+// Write other function prototypes here (if any)
 
 
 int main()
@@ -731,10 +740,115 @@ corresponds to the index of the array.
     struct digit * numberptr;
     int digitToStore = 5;
     numberptr = createDigit(digitToStore);
-    printf("We are storing the digit %d and the pointer %p at memory location %p.\n", numberptr->num, numberptr->next, numberptr);
+    printf("We are storing the digit %d and the pointer %p at memory \
+           location %p.\n", numberptr->num, numberptr->next, numberptr);
     free(numberptr);
 
 
+
+/*
+
+You would like to store student data (for each student, their name and age) in a linked list of students.
+You are given the following structure to store each student's information. Please do not modify this structure:
+
+struct student {
+      char name[50];
+      int age;
+      struct student *next;
+};
+Your first task is to write a function createStudent() that takes as inputs a string (namely a student's name)
+and an integer (their age) and that returns a pointer to a struct student which stores this information.
+ Your function should dynamically allocate the memory required for this struct student and then write
+  the student's name and age into this newly allocated memory.
+
+You are given the createStudent() function prototype and a main() function to test your code, please do not modify these:
+
+struct student *createStudent(char studentName[], int studentAge);
+
+int main(void) {
+    struct student *studptr;
+    int myAge;
+    char myName[50];
+    scanf("%s %d", myName, &myAge);
+    studptr = createStudent(myName, myAge);
+    printf("New student created: %s is %d years old.\n", studptr->name, studptr->age);
+    free(studptr);
+    return 0;
+}
+Examples
+Input:
+Petra 25
+Output:
+New student created: Petra is 25 years old.
+Input:
+Remi 18
+Output:
+New student created: Remi is 18 years old.
+Warning: You will be graded on your output, so do not include any print statements that prompt a user for input.
+
+Note
+You may submit as many solutions as you wish. Only your highest score will be kept.
+
+This task offers 1 hint :
+
+Hint 1 :
+
+It may be useful to create a function copyStr() which copies one string to another string.
+
+
+solution :-
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student {
+      char name[50];
+      int age;
+      struct student *next;
+};
+
+struct student *createStudent(char studentName[], int studentAge);
+void copyStr(char *source, char *target);
+
+int main(void) {
+    struct student *studptr;
+    int myAge;
+    char myName[50];
+    scanf("%s %d", myName, &myAge);
+    studptr = createStudent(myName, myAge);
+    printf("New student created: %s is %d years old.\n", studptr->name, studptr->age);
+    free(studptr);
+    return 0;
+}
+
+struct student *createStudent(char studentName[], int studentAge) {
+    struct student *ptr;
+    ptr = (struct student *) malloc(sizeof(struct student));
+    copyStr(studentName, ptr->name);
+    ptr->age = studentAge;
+    ptr->next = NULL;
+    return ptr;
+}
+/// write a function to convert array of shar into a string in c
+void copyStr(char *source, char *target) {
+    int i = 0;
+    while (source[i]!='\0') {
+        target[i] = source[i];
+        i++;
+    }
+    target[i] = '\0';
+}
+
+*/
+
+    struct student *studptr;
+    int myAge;
+    char myName[50];
+    scanf("%s %d", myName, &myAge);
+    studptr = createStudent(myName, myAge);
+    printf("New student created: %s is %d years old.\n", studptr->name, studptr->age);
+    free(studptr);
+    return 0;
 
 
 
@@ -1016,5 +1130,33 @@ struct digit * createDigit(int dig) {
     return ptr;
 }
 
+// Write your createStudent function here (and any other functions you see fit)
+struct student *createStudent(char studentName[], int studentAge)
+{
+    int i = 0;
+    struct student *CreateStudent;
+    char StudentName[50];
+    CreateStudent = (struct student *) malloc(sizeof(struct student));
+    while(studentName[i]!= NULL) https://overiq.com/c-programming-101/array-of-pointers-to-strings-in-c/
+    { CreateStudent->name[i]=studentName[i]; i=i+1; } /// write a function to convert array of shar into a string in c
+
+/// As you can see not all strings are long enough to fill all the rows of the array, that's why compiler fills these empty spaces (highlighted using light grey color) with the null characters ('\0').
+    CreateStudent->age = studentAge;
+
+    return CreateStudent;
+}
+
+/// write a function to convert array of shar into a string in c
+
+void copyStr(char *source, char *target) {
+    int i = 0;
+    while (source[i]!='\0') /// https://overiq.com/c-programming-101/array-of-pointers-to-strings-in-c/
+    /// As you can see not all strings are long enough to fill all the rows of the array, that's why compiler fills these empty spaces (highlighted using light grey color) with the null characters ('\0').
+        {
+        target[i] = source[i];
+        i++;
+    }
+    target[i] = '\0';
+}
 
 
