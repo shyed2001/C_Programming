51f2c456134e7c7812e19eb40cbbd35a73d7ce5d
shyed2001 <shyed2001@yahoo.com>
2021-09-06 15:00:42 +0600

    ok
    ok

--- C_Programming_UsingLinuxToolsandLibraries/program.c
+++ C_Programming_UsingLinuxToolsandLibraries/program.c
@@ -1,7 +1,9 @@
     #include <stdio.h>
     #include <math.h>
     /// double pow(double, double); 
-    int main(){
+    int main()
+    
+    {
         double a = 2.5;
         double asquared = pow(a,2);
         printf("%.2lf squared = %.2lf\n",a,asquared);
